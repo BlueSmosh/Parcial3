d6ab6223d5d1a73808dbb9a3524618a6cf032d58
Santiago G Gallego <50247372+BlueSmosh@users.noreply.github.com>
2025-04-22 19:30:31 -0500

    Clase Y Controlodar Eventos

---
+++ Clases/clsEventos.cs
@@ -0,0 +1,88 @@
+﻿using Parcial3.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
+using System.Data.Entity.Migrations;
+using System.Linq;
+using System.Web;
+
+namespace Parcial3.Clases
+{
+    public class clsEventos
+    {
+        private DBExamen3Entities dbExamen = new DBExamen3Entities();
+
+        public Evento evento { get; set; }
+
+        public string Insertar()
+        {
+            try
+            {
+                dbExamen.Eventos.Add(evento);
+                dbExamen.SaveChanges();
+                return "Evento registrado con exito";
+            }
+            catch (Exception ex)
+            {
+                return "Error al registrar el evento: " + ex.Message;
+
+            }
+        }
+
+        public string Actualizar()
+        {
+            try
+            {
+               
+                Evento even = Consultar(evento.TipoEvento);
+                if (even == null)
+                {
+                    return "evento no existe";
+                }
+
+                dbExamen.Eventos.AddOrUpdate(evento); 
+                dbExamen.SaveChanges(); 
+                return "evento actualizado correctamente";
+            }
+            catch (Exception ex)
+            {
+                return "Error al actualizar el evento: " + ex.Message; 
+            }
+        }
+
+        public Evento Consultar(string id)
+        {
+
+            Evento empLeado = dbExamen.Eventos.FirstOrDefault(e => e.TipoEvento == id);
+            return empLeado;
+        }
+
+        public string Eliminar()
+        {
+
+            try
+            {
+                Evento even = Consultar(evento.TipoEvento);
+                if (even == null)
+                {
+                    return "El evento No Existe";
+                }
+          
+                dbExamen.Eventos.Remove(even);
+                dbExamen.SaveChanges();
+                return "evento eliminado correctamente";
+            }
+            catch (Exception ex)
+            {
+                return "error al eliminar el evento" + ex.Message;
+            }
+        }
+
+        public List<Evento> ConsultarTodosEventos()
+        {
+            return dbExamen.Eventos
+                .OrderBy(e => e.idEventos)
+                .ToList();
+        }
+    }
+}
---
+++ Controllers/EventosController.cs
@@ -0,0 +1,55 @@
+﻿using Parcial3.Clases;
+using Parcial3.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Parcial3.Controllers
+{
+    [RoutePrefix("api/Eventos")]
+    public class EventosController : ApiController
+    {
+        [HttpGet]
+        [Route("ConsultarTodosEventos")]
+        public List<Evento> ConsultarTodosEventos()
+        {
+            clsEventos Evento = new clsEventos();
+            return Evento.ConsultarTodosEventos();
+
+        }
+        [HttpGet]
+        [Route("ConsultarPorEvento")]
+        public Evento ConsultarPorId(string id)
+        {
+            clsEventos Evento = new clsEventos();
+            return Evento.Consultar(id);
+        }
+        [HttpPost]
+        [Route("Insertar")]
+        public string Insertar([FromBody] Evento evento)
+        {
+            clsEventos Evento = new clsEventos();
+            Evento.evento = evento;
+            return Evento.Insertar();
+        }
+        [HttpPut]
+        [Route("Actualizar")]
+        public string Actualizar([FromBody] Evento evento)
+        {
+            clsEventos objEvento = new clsEventos();
+            objEvento.evento = evento;
+            return objEvento.Actualizar();
+        }
+        [HttpDelete]
+        [Route("Eliminar")]
+        public string Eliminar([FromBody] Evento evento)
+        {
+            clsEventos objEvento = new clsEventos();
+            objEvento.evento = evento;
+            return objEvento.Eliminar();
+        }
+    }
+}
